6f5c447db0c4b1e5c20f6028d4719d350de76356
Matthew McKee <matthew@demandwell.com>
2022-07-11 14:59:04 -0400

    Add Cart, LineItem, add counter for store visit.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,14 @@
 class StoreController < ApplicationController
+  before_action :session_counter
+
+  def session_counter
+    if session[:session_counter].nil?
+      session[:session_counter] = 0
+    end
+
+    session[:session_counter] += 1
+    @session_counter = session[:session_counter]
+  end
   def index
     @products = Product.order(:title)
   end
